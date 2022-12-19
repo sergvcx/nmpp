8fe16c61d02709c0df0896e8b4df6e001b33a829
serg <mushkaev@module.ru>
2022-12-19 16:00:56 +0300

    risc.mk

--- app/test/float/arithmetic/nmppsAdd_32f/main.cpp
+++ app/test/float/arithmetic/nmppsAdd_32f/main.cpp
@@ -1,5 +1,7 @@
 #include <nmpp.h>
 #define PI 3.14
+
+#include "stdio.h"
 int main()
 {
 	
---
+++ make/src-nmc-risc.mk
@@ -0,0 +1,8 @@
+DIRS+=$(ROOT)/src/risc/fixpoint32
+DIRS+=$(ROOT)/src/risc/fixpoint64 
+DIRS+=$(ROOT)/src/risc/init 
+DIRS+=$(ROOT)/src/risc/linear 
+DIRS+=$(ROOT)/src/risc/metric 
+DIRS+=$(ROOT)/src/risc/support 
+DIRS+=$(ROOT)/src/risc/tables 
+
