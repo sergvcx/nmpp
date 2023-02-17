5622413dca55b52ce0efe09575bd86766cf74f95
serg <mushkaev@module.ru>
2023-02-17 20:25:02 +0300

    float examples half ready

---
+++ examples/float/nmblas/nmblas_drotm/main.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "nmblas.h"
+
+
+#define SIZE 10
+#define PI 3.1415
+
+#ifdef __GNUC__ // NMC-GCC C++ compilier 
+double x[SIZE] __attribute__ ((section (".data_imu1")));
+double y[SIZE] __attribute__ ((section (".data_imu2")));
+#else 			// NMSDK C++ compiler
+#pragma data_section ".data_imu1"
+double x[SIZE];
+#pragma data_section ".data_imu2"
+double y[SIZE];
+#endif 
+
+double param[5];
+int main(){
+	int i;
+
+	param[1] = 0.707;//h11
+	param[2] = -0.707;//h21
+	param[3] = 0.707;//h12
+	param[4] = 0.707;//h22
+
+	// DROTM - BLAS level one. Applies a modified Givens rotation.
+
+	for(int i=0;i<SIZE;i++){
+		x[i] = 10*sin(PI/6*i);
+		y[i] = 10*cos(PI/6*i);
+		printf("x[%d]=%f\ty[%d]=%f\n",i,x[i],i,y[i]);
+	}
+	printf("\n");
+
+	param[0] = -2; 
+	printf("PARAM is -2\n");
+	nmblas_drotm(SIZE,x,1,y,1,param);
+
+	for(int i=0;i<SIZE;i++){
+		printf("x[%d]=%f\ty[%d]=%f\n",i,x[i],i,y[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
+	
---
+++ examples/float/nmblas/nmblas_dscal/main.cpp
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <nmblas.h>
+#include "nmpp.h"
+
+#define SIZE 16
+#ifdef __GNUC__ //  NMC-GCC C++ compilier 
+double buffer_a[SIZE] __attribute__ ((section (".data_imu1")));
+#else 		// NMSDK C++ compiler
+#pragma data_section ".data_imu1"
+double buffer_a[SIZE];
+
+#endif 
+/*
+NAME
+       DSCAL - BLAS level one, scales a double precision vector
+
+SYNOPSIS
+       SUBROUTINE DSCAL    ( n, alpha, x, incx )
+
+           INTEGER           n, incx
+
+           DOUBLE PRECISION  alpha, x
+
+
+DESCRIPTION
+       DSCAL  scales a double precision vector with a double precision scalar.
+       DSCAL scales the vector x of length n and increment incx  by  the  con-
+       stant a.
+
+       This routine performs the following vector operation:
+
+                 x <-- alpha x
+
+       where alpha is a double precisoin scalar, and x is a double precision
+       vector.
+
+ARGUMENTS
+       n       INTEGER. (input)
+               Number of elements in the vector.
+               If n <= 0, this routine returns without computation.
+
+       alpha   DOUBLE PRECISION scalar alpha. (input)
+
+       x       DOUBLE PRECISION, (input and output)
+               Array of dimension (n-1) * |incx| + 1. Vector to be scaled.
+
+       incx    INTEGER. (input)
+               Increment between elements of x.
+               If incx = 0, the results will be unpredictable.
+
+NOTES
+       This  routine  is  Level  1  Basic  Linear Algebra Subprograms (Level 1
+       BLAS).
+
+       When working backward (incx < 0 or incy < 0), each  routine  starts  at
+       the end of the vector and moves backward, as follows:
+
+            x(1-incx * (n-1)), x(1-incx * (n-2)), ..., x(1)
+*/			
+int main(){
+	printf("Input:\n");
+	for(int i=0;i<SIZE;i++){
+		buffer_a[i] = i;	
+		printf("%f\n",buffer_a[i]);
+	}
+	// DSCAL scales a vector by a constant.
+	
+	// ------ case 1: x2.0-----------
+	nmblas_dscal(SIZE,2.0,buffer_a,1);
+	
+	printf("Result case 1:\n");
+	for(int i=0;i<SIZE;i++){
+		printf("%f\n",buffer_a[i]);
+	}
+	printf("Result case 2:x2.0\n");
+	for(int i=0;i<SIZE;i++){
+		printf("%f\n",buffer_a[i]);
+	}
+	
+	// ------ case 2 -----------
+	printf("Result case 2 :x10.0\n");
+	
+	nmblas_dscal(SIZE/2,10,buffer_a,2);
+	for(int i=0;i<SIZE;i++){
+		printf("%f\n",buffer_a[i]);
+	}
+	
+	// ------ case 3 -----------
+	printf("Result case 3: x0.1 \n");
+	
+	nmblas_dscal(SIZE/2,0.1,buffer_a+SIZE-1,-2);
+	for(int i=0;i<SIZE;i++){
+		printf("%f\n",buffer_a[i]);
+	}
+	
+	// ------ case 4 -----------
+	printf("Result case 3: x0.5 \n");
+	
+	nmblas_dscal(SIZE,0.5,buffer_a+SIZE-1,-1);
+	for(int i=0;i<SIZE;i++){
+		printf("%f\n",buffer_a[i]);
+	}
+	
+	return 0;
+}
+
